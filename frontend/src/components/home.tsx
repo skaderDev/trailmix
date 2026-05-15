60302152ab1e52f873c2999193fca9734b464e99
safwan <skadeny@gmail.com>
2026-05-14 22:23:46 -0400

    added front page and stylized

---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Home() {
+
+    return (
+        <>
+        <div className="home flex flex-col items-center justify-center min-h-screen">
+            <h1>TrailMix</h1>
+            <button>Get Started</button>
+        </div>
+        </>
+    )
+}
